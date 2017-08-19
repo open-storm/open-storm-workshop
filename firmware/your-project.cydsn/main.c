f52b11aeef5a53b00294c7d4045debbd49aa8873
Matt Bartos <mdbartos@umich.edu>
2017-08-19 03:26:40 -0400

    Add user project

---
+++ firmware/your-project.cydsn/main.c
@@ -0,0 +1,16 @@
+#include <project.h>
+
+int main(void)
+{
+    // Startup code goes here
+    
+    // Enable global interrupts
+    CyGlobalIntEnable;
+    
+    for(;;)
+    {
+        // Main routine goes here
+    }
+}
+
+/* [] END OF FILE */
